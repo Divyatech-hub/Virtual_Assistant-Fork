5ef57fca3228f9077a8ce4ba1ac1ef863b3cd1b7
Shan-234 <shantanu.bti@gmail.com>
2024-09-25 15:10:48 -0400

    Integrated Google Studio API with the project and enabled response display for every text input from the user.

--- src/BotMessage.js
+++ src/BotMessage.js
@@ -1,9 +1,28 @@
 import React, { useState, useEffect } from "react";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export default function BotMessage(props) {
+    const [response, SetResponse] = useState("Loading...");
+    const genAI = new GoogleGenerativeAI("AIzaSyA7AJ93pbecmz9s3pUyxhskybYBwSPXoJk");
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    useEffect(() => {
+        const GetResponse = async () => {
+            try {
+                const result = await model.generateContent(props.prompt + "(Generate answer in regular text and not Markdown)");
+                console.log(result);
+                SetResponse(result.response.text());
+            } catch (err) {
+                SetResponse("" + err);
+            }
+        };
+
+        GetResponse();
+    }, []);
+
     return (
         <div class="message bot">
-            <div class="message-content">{props.prompt}</div>
+            <div class="message-content">{response}</div>
         </div>
     );
 }
